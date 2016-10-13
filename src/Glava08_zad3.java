c111697ea9c7f35664f370fc435787fcf9af978d
b0dry <dobrydinkov@yahoo.com>
2016-10-13 18:44:17 +0300

    Bug Fixing

--- src/Glava08_zad3.java
+++ src/Glava08_zad3.java
@@ -6,11 +6,12 @@ public class Glava08_zad3 {
 		int[] arr1 = { 5, 4, 3, 2, 1, 23, 2323, 23233, 4545, 656, 778, 8996, 34456, 34343 };
 		int[] arr2 = { 5, 4, 3, 2, 1, 23, 2323 };
 		int[] arr = DL.concatArrays(arr1, arr2);
-		long result;
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
+		for (int anArr : arr) {
+			System.out.print(anArr);
 			System.out.print("; ");
 		}
+		System.out.println();
+		long result;
 		for (int counter = -5; counter <= 15; counter++) {
 			result = DL.fibonacci(counter);
 			if (result != 0)
