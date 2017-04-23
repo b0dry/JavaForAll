d6f65db145efee061c9b0fd5224b1ad6e241fa88
dobromirdinkov <dobrydinkov@yahoo.com>
2017-04-23 09:37:38 +0300

    Commit the new class from hackerrank Day 24 - More Linked Lists

---
+++ src/hackerrank/Testing.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+/**
+ * Created by DDinkov on 14-Apr-17 5:20 PM.
+ * https://www.hackerrank.com/challenges/30-testing
+ */
+
+public class Testing {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
